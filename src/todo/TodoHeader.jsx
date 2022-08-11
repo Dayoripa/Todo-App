987c6d6883e759fd5d86b990a7e0c2b6d0fb6914
Daulin Riascos <daulinri@gmail.com>
2022-08-10 21:59:14 -0500

    se agrega estilos al header

--- src/todo/TodoHeader.jsx
+++ src/todo/TodoHeader.jsx
@@ -1,9 +1,12 @@
-
+import iconSun from '../assets/images/icon-sun.svg';
 
 export const TodoHeader = () => {
   return (
     <header className='hero'>
+      <div className='hero__container'>
         <h1 className='hero__title'>Todo</h1>
+        <img className='hero__img' src={iconSun} alt="" />
+      </div>
     </header>
 
   )
